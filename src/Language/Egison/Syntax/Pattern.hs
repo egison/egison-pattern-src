8ad674a7ec46f981bd22dd7308848914d55ed8b1
coord.e <me@coord-e.com>
2020-02-12 15:37:33 +0900

    Add: Initialize a 'egison-pattern' package

---
+++ src/Language/Egison/Syntax/Pattern.hs
@@ -0,0 +1,3 @@
+module Language.Egison.Syntax.Pattern
+  ( )
+where
---
+++ test/Doctest.hs
@@ -0,0 +1,14 @@
+import           Build_doctests                 ( flags
+                                                , pkgs
+                                                , module_sources
+                                                )
+import           Data.Foldable                  ( traverse_ )
+import           System.Environment             ( unsetEnv )
+import           Test.DocTest                   ( doctest )
+
+main :: IO ()
+main = do
+  traverse_ putStrLn args -- optionally print arguments
+  unsetEnv "GHC_ENVIRONMENT" -- see 'Notes'; you may not need this
+  doctest args
+  where args = flags ++ pkgs ++ module_sources
