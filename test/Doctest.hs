af98cec67d62f20df0313180baf42bd217c6dad4
coord.e <me@coord-e.com>
2020-02-15 14:18:58 +0000

    Change: Suppress verbose message from doctest driver

--- test/Doctest.hs
+++ test/Doctest.hs
@@ -2,13 +2,9 @@ import           Build_doctests                 ( flags
                                                 , pkgs
                                                 , module_sources
                                                 )
-import           Data.Foldable                  ( traverse_ )
 import           System.Environment             ( unsetEnv )
 import           Test.DocTest                   ( doctest )
 
 main :: IO ()
-main = do
-  traverse_ putStrLn args -- optionally print arguments
-  unsetEnv "GHC_ENVIRONMENT" -- see 'Notes'; you may not need this
-  doctest args
+main = unsetEnv "GHC_ENVIRONMENT" >> doctest args
   where args = flags ++ pkgs ++ module_sources
