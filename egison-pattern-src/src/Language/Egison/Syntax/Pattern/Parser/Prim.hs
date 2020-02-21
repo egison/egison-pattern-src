72269b4cd92683dc02363715dc15e045aac20d63
coord.e <me@coord-e.com>
2020-02-21 05:25:37 +0000

    Change: Separate 'ParseMode'

--- egison-pattern-src/src/Language/Egison/Syntax/Pattern/Parser/Prim.hs
+++ egison-pattern-src/src/Language/Egison/Syntax/Pattern/Parser/Prim.hs
@@ -89,10 +89,6 @@ import qualified Text.Megaparsec.Char.Lexer    as L
                                                 , space
                                                 )
 
-import           Language.Egison.Syntax.Pattern.Parser.Associativity
-                                                ( Associativity )
-import           Language.Egison.Syntax.Pattern.Parser.Precedence
-                                                ( Precedence )
 import qualified Language.Egison.Syntax.Pattern.Parser.Token
                                                as Token
                                                 ( isSpace
@@ -120,27 +116,11 @@ import           Language.Egison.Syntax.Pattern.Parser.Prim.Source
                                                 )
 import           Language.Egison.Syntax.Pattern.Parser.Prim.Location
                                                 ( fromSourcePos )
-
-
--- | @'ExtParser' s a' is a type for externally provided parser of @a@
-type ExtParser s a = Tokens s -> Either String a
-
--- | Fixity of infix operators.
-data Fixity n s =
-  Fixity { associativity :: Associativity
-         , precedence :: Precedence
-         , parser :: ExtParser s n
-         }
-
--- | Parser configuration.
-data ParseMode n v e s
-  = ParseMode { fixities        :: [Fixity n s]
-              , blockComment    :: Maybe (Tokens s, Tokens s)
-              , lineComment     :: Maybe (Tokens s)
-              , varNameParser   :: ExtParser s v
-              , nameParser      :: ExtParser s n
-              , valueExprParser :: ExtParser s e
-              }
+import           Language.Egison.Syntax.Pattern.Parser.Prim.ParseMode
+                                                ( ParseMode(..)
+                                                , Fixity(..)
+                                                , ExtParser
+                                                )
 
 
 -- | A parser monad.
